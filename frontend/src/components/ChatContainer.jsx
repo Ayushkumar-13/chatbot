2817d202dc57bf554e7302245e9b3d099324e88d
Ayush Kumar <ayushkumar6064@gmail.com>
2025-08-02 21:05:20 +0530

    completed

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -58,7 +58,7 @@ const ChatContainer = () => {
                     {
                         selectedUser.fullName
                     }
-                  {onlineUsers.includes(selectedUser._id)} <span className='w-2 h-2 rounded-full bg-green-500 '> </span>
+                  {onlineUsers.includes(selectedUser._id) && <span className='w-2 h-2 rounded-full bg-green-500 '> </span>}
                 </p>
 
                 <img onClick={() => setSelectedUser(null)} src={assets.arrow_icon} className='md:hidden max-w-7' alt="" />
